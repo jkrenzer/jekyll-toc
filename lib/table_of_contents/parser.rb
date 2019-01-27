d2137bb0b3aca16841d058de82a5d203bde6840b
Jörn Krenzer <joern@krenzer.space>
2019-01-27 19:35:24 +0100

    Added different classes for each level

--- lib/table_of_contents/parser.rb
+++ lib/table_of_contents/parser.rb
@@ -14,7 +14,8 @@ module Jekyll
         'list_class' => 'section-nav',
         'sublist_class' => '',
         'item_class' => 'toc-entry',
-        'item_prefix' => 'toc-'
+        'item_prefix' => 'toc-',
+	'link_class' => ''
       }.freeze
 
       def initialize(html, options = {})
@@ -25,7 +26,15 @@ module Jekyll
         @list_class = options['list_class']
         @sublist_class = options['sublist_class']
         @item_class = options['item_class']
+	if @item_class.is_a?(Array)
+  	  @item_class.at(1) ||= @sublist_class
+	elsif @item_class.is_a?(Hash)
+	  @item_class.at(1) ||= @sublist_class
+	else
+	  @item_class = [ @item_class , @sublist_class]
+	end
         @item_prefix = options['item_prefix']
+	@link_class = options['link_class']
         @entries = parse_content
       end
 
@@ -80,21 +89,25 @@ module Jekyll
       end
 
       # Returns the list items for entries
-      def build_toc_list(entries)
+      def build_toc_list(entries, level=0)
         i = 0
         toc_list = +''
         min_h_num = entries.map { |e| e[:h_num] }.min
+	# Enforce Level >= 0
+	if level < 0
+	  level = 0
+	end
 
         while i < entries.count
           entry = entries[i]
           if entry[:h_num] == min_h_num
             # If the current entry should not be indented in the list, add the entry to the list
-            toc_list << %(<li class="#{@item_class} #{@item_prefix}#{entry[:node_name]}"><a href="##{entry[:id]}#{entry[:uniq]}">#{entry[:text]}</a>)
+            toc_list << %(<li class="#{lvl_opt_val(@item_class, level, DEFAULT_CONFIG['item_class'])} #{@item_prefix}#{entry[:node_name]}"><a #{lvl_attr(@link_class, level, DEFAULT_CONFIG['link_class'])}href="##{entry[:id]}#{entry[:uniq]}">#{entry[:text]}</a>)
             # If the next entry should be indented in the list, generate a sublist
             next_i = i + 1
             if next_i < entries.count && entries[next_i][:h_num] > min_h_num
               nest_entries = get_nest_entries(entries[next_i, entries.count], min_h_num)
-              toc_list << %(\n<ul#{ul_attributes}>\n#{build_toc_list(nest_entries)}</ul>\n)
+              toc_list << %(\n<ul #{lvl_attr(@list_class, level + 1, DEFAULT_CONFIG['sublist_class'])}>\n#{build_toc_list(nest_entries, level + 1)}</ul>\n)
               i += nest_entries.count
             end
             # Add the closing tag for the current entry in the list
@@ -144,8 +157,46 @@ module Jekyll
         DEFAULT_CONFIG
       end
 
-      def ul_attributes
-        @ul_attributes ||= @sublist_class.empty? ? '' : %( class="#{@sublist_class}")
+      def lvl_opt_val(option, level=0, default=nil)
+	# Enforce Level >= 0
+	if !level.is_a?(Integer) or level < 0
+          level = 0
+        end
+	# If the option is an array, we try to get the appropriate string by taking the level-th element
+        if option.is_a?(Array)
+          if option.length > level and option[level] != nil
+	    return option[level]
+	  elsif option.last != nil
+            return option.last
+	  else
+	    return default
+	  end
+	# Is the option a hash, we look up level in the keys
+	elsif option.is_a?(Hash)
+	  opt_val = nil
+	  option.each do |key, value|
+            # If we got a string: Cast to int
+            if key.is_a?(String)
+              key = key.to_i
+	    end
+	    if key.is_a?(Integer) and key <= level
+              opt_val = value
+	    end
+	  end
+          return opt_val == nil ? default : opt_val
+	else
+	  return option == nil ? default : option
+	end
+      end
+      def lvl_attr(name, option, level=0, default=nil)
+        unless name.is_a?(String)
+	  return ''
+	end
+        opt_val = lvl_opt_val(option, level, default)
+	unless opt_val == nil or opt_val == ''
+	  return "#{name}=\"#{opt_val}\" "
+	else
+          return ''
       end
     end
   end
